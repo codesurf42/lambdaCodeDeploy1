9d81194e2f584ce9a1273c5b6803ee813af1906d
adamb <adamdd55@gmail.com>
2017-09-06 13:07:39 +0100

    abc 2

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-// version abc
+// version abc 2
 var time = require('time');
 exports.handler = (event, context, callback) => {
     var currentTime = new time.Date(); 
     currentTime.setTimezone("Europe/London");
     callback(null, {
         statusCode: '200',
-        body: 'The time in Brighton is: ' + currentTime.toString(),
+        body: 'The time in Brighton area is: ' + currentTime.toString(),
     });
 };
